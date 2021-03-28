661b06c4d8ae91b17beb2b7c3edb486b6d73fc9f
sadin1106 <64366727+sadin1106@users.noreply.github.com>
2021-03-28 17:57:59 +0700

    UI/UX

---
+++ app/src/main/java/com/a1_1801040061/mprtut08_1801040061/MainActivity.java
@@ -0,0 +1,14 @@
+package com.a1_1801040061.mprtut08_1801040061;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
